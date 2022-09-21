9e5d3804740e643a39e65c24594ee489c30c1c62
fknol309 <fknol309@gmail.com>
2022-09-21 07:10:03 -0400

    added continue and added fileout to template

---
+++ day14/census_template.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // open census data file
+
+    // try to read the file to be written to
+    // check to see if file already exists
+
+    // create file to be written to
+    // check to see if file was created
+
+    // read in (and ignore!) header information
+
+    // loop over each row
+    {
+        // process row
+    }
+
+    return 0;
+}
