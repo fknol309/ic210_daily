79a06c7f4b7f6f2eea5f2fa6a568ff97a5fc7402
fknol309 <fknol309@gmail.com>
2022-10-03 07:59:51 -0400

    added repeat.cpp

---
+++ day16/repeat.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cinttypes>
+using namespace std;
+
+/* Prototypes */
+void repeat(char, int);
+
+
+/* Main */
+int main()
+{
+    //declare variables
+    char c;
+    int n;
+
+    cout << "Character to be repeated: ";
+    cin >> c;
+
+    cout << "Number of times to be repeated: ";
+    cin >> n;
+
+    // repeat c, n times
+    repeat(c, n);
+
+    return 0;
+}
+
+
+/* Function Definition */
+void repeat(char c, int n) {
+
+
+
+
+}
