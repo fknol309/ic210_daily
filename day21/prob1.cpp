754707510ede6a2beb7d8ba0e411aec829c063d7
fknol309 <fknol309@gmail.com>
2022-10-16 13:30:43 -0400

    day21

---
+++ day21/prob1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // read size n, allocate array of n doubles, read values into array
+    int n;
+    cin >> n;
+
+    // create array of size n
+
+
+    // read in values from terminal
+    for(int i = 0; i < n; i++) {
+    
+
+    
+    }
+
+    // find minimum, maximum, average
+
+
+    // calculate counts of values
+
+
+    // create and print array of differences (A[i] - A[i-1])    
+    // of consecutive elements
+
+
+
+    // print out diffs and array elements
+    // 3  5  -1
+    // \_/ \_/
+    //  2  -6
+
+
+    return 0;
+}
